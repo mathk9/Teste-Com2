74705ff1801b12ecdcbd0375eafd6fd3b5f02a1f
matheus <math.novais@outlook.com>
2020-11-03 13:12:29 -0300

    03/11/2020

---
+++ TESTE_COM_2/Conection.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace TESTE_COM_2
+{
+    /// <summary>
+    /// Interaction logic for Conection.xaml
+    /// </summary>
+    public partial class Conection : Window
+    {
+        public Conection()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Window1 d = new Window1();
+                
+                d.taxaTrans = Convert.ToInt16(cb_Taxa.Text);
+                d.Comunicacao = cb_Porta.SelectedItem.ToString();
+
+                if (rB_7bits.IsChecked == true)
+                    d.bits = 7;
+                if (rB_8bits.IsChecked == true)
+                    d.bits = 8;
+
+                if (rB_1.IsChecked == true)
+                    d.bitParada = 1;
+                if (rB_2.IsChecked == true)
+                    d.bitParada = 2;
+
+                if (rB_Sem.IsChecked == true)
+                    d.pariedade = "Sem";
+
+                d.roda = true;
+                d.StarTimer();
+
+                this.Close();
+            }
+            catch { }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            btn_Ok.Focus();
+
+            rB_Sem.IsChecked = true;
+            rB_8bits.IsChecked = true;
+            rB_1.IsChecked = true;
+            rB_RTU.IsChecked = true;
+            try
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    cb_Porta.Items.Add(s);
+                }
+                cb_Porta.SelectedIndex = 0;
+            }
+            catch { }
+        }
+
+        private void btn_Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TESTE_COM_2/Window1.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FtdAdapter;
+using Modbus.Device;
+using System.Data;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Threading;
+
+namespace TESTE_COM_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public string Comunicacao;
+        public int taxaTrans;
+        public int bits;
+        public int bitParada;
+        public string pariedade;
+        public byte slaveId;
+        public ushort startAddress;
+        public ushort numRegisters;
+        public bool roda;
+
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void StarTimer()
+        {
+            dispatcherTimer.Tick += TimerTick;
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(Convert.ToInt16(txt_Tempo.Text));
+            
+            dispatcherTimer.Start();
+            slaveId = Convert.ToByte(txt_ID.Text);
+            startAddress = ushort.Parse(txt_End.Text);
+            numRegisters = ushort.Parse(txt_Tamanho.Text);
+        }
+
+        private void MenuArquivo_Click(object sender, RoutedEventArgs e)
+        {
+            Conection tela = new Conection();
+            tela.ShowDialog();
+        }
+
+        List<string> lista = new List<string>();
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            
+            if (roda == true)
+            {
+                using (SerialPort port = new SerialPort(Comunicacao))
+                {
+                    // configure serial port
+                    port.BaudRate = taxaTrans;
+                    port.DataBits = bits;
+                    if (pariedade == "Sem")
+                    {
+                        port.Parity = Parity.None;
+                    }
+                    if (bitParada == 1)
+                    {
+                        port.StopBits = StopBits.One;
+                    }
+                    if (bitParada == 2)
+                    {
+                        port.StopBits = StopBits.Two;
+                    }
+                    port.Open();
+
+                    // create modbus master
+                    IModbusSerialMaster master = ModbusSerialMaster.CreateRtu(port);
+
+                    master.Transport.ReadTimeout = 300;
+                    // read five registers		
+                    ushort[] registers = master.ReadHoldingRegisters(slaveId, startAddress, numRegisters);
+
+
+                    for (int i = 0; i < numRegisters; i++)
+                    {
+                        lista.Add("Register " + (startAddress + i) + (registers[i]));
+
+                    }
+                }
+            }
+            listBox1.Items.Add(lista);
+        }
+    }
+}
